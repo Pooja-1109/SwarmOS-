0dcbdb75e1145a4bf38599d6336ef0a192f4f6aa
Pooja-1109 <poojaadmane1109@gmail.com>
2026-08-01 23:58:20 +0530

    Initial setup: React + TypeScript + Tailwind CSS

---
+++ OneDrive/Documents/btechpooja/swarmOS/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-6xl font-extrabold text-white">
+        Welcome to SwarmOS 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
